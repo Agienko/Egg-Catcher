a2ec0d344193bf8fa1fe9f834bce4d1f8d7dac98
yuriiahiienko <yurii.ahiienko@goreel.tech>
2026-01-01 17:27:45 +0100

    refactoring

--- src/game/components/chicken.js
+++ src/game/components/chicken.js
@@ -2,6 +2,26 @@ import {AnimatedSprite, Texture} from "pixi.js";
 import {Egg} from "./egg.js";
 import {SIGNALS} from "../signals/signals.js";
 
+
+const SHIT_CHANCES = [
+    { score: 1000, chance: 0.95 },
+    { score: 750,  chance: 0.9 },
+    { score: 500,  chance: 0.85 },
+    { score: 300,  chance: 0.8 },
+    { score: 200,  chance: 0.75 },
+    { score: 100,  chance: 0.7 }
+];
+
+
+const BONUS_CHANCES = [
+    { score: 1000, chance: 0.65 },
+    { score: 750, chance: 0.7 },
+    { score: 500, chance: 0.75 },
+    { score: 300, chance: 0.8 },
+    { score: 200, chance: 0.85 },
+    { score: 100, chance: 0.9 },
+]
+
 export class Chicken extends AnimatedSprite{
     constructor(stage, i) {
         super({textures: ['chicken_1', 'chicken_2'].map(name => Texture.from(name))});
@@ -18,48 +38,18 @@ export class Chicken extends AnimatedSprite{
     }
     #generateShit(){
         const score = SIGNALS.score.value;
-
-        let chance = 0.75;
-
-        if(score > 1000) {
-            chance = 0.95;
-        } else if(score > 750){
-            chance = 0.9;
-        } else if(score > 500) {
-            chance = 0.85;
-        } else if(score > 300) {
-            chance = 0.8;
-        } else if(score > 200) {
-            chance = 0.75;
-        } else if(score > 100) {
-            chance = 0.7;
-        }
-
+        const chance = SHIT_CHANCES
+            .find(t => score > t.score) ?? 0.75;
         return Math.random() > chance;
     }
     #generateBonus(){
         const score = SIGNALS.score.value;
-
-        let chance = 0.95;
-
-        if(score > 1000) {
-            chance = 0.65;
-        }else if(score > 750){
-            chance = 0.7;
-        }else if(score > 500) {
-            chance = 0.75;
-        } else if(score > 300) {
-            chance = 0.80;
-        } else if(score > 200) {
-            chance = 0.85;
-        } else if(score > 100) {
-            chance = 0.9;
-        }
-
+        const chance = BONUS_CHANCES
+            .find(t => score > t.score) ?? 0.95;
         return Math.random() > chance;
     }
 
-    getEgg(){
+    createEgg(){
         this.gotoAndPlay(1);
 
         const isShit = this.#generateShit();
--- src/game/game.js
+++ src/game/game.js
@@ -3,12 +3,26 @@ import {World} from "./components/world.js";
 import {Texts} from "./components/texts.js";
 import {Chicken} from "./components/chicken.js";
 import {Bag} from "./components/bag.js";
-import {getRandomInt, randomFromArr} from "../helpers/helper.js";
-import {gsap} from "gsap";
-import {effect} from "@preact/signals-core";
-import {SIGNALS} from "./signals/signals.js";
 import {GameOver} from "../game-over/game-over.js";
+import {getRandomInt, randomFromArr} from "../helpers/helper.js";
+import {effect} from "@preact/signals-core";
+import {gsap} from "gsap";
+import {SIGNALS} from "./signals/signals.js";
 import {flashBack} from "../main.js";
+
+const DELAY_THRESHOLDS = [
+    { score: 1000, min: 180, max: 250 },
+    { score: 750,  min: 200, max: 290 },
+    { score: 500,  min: 220, max: 300 },
+    { score: 350,  min: 250, max: 400 },
+    { score: 250,  min: 270, max: 450 },
+    { score: 180,  min: 280, max: 500 },
+    { score: 120,  min: 290, max: 550 },
+    { score: 80,   min: 300, max: 600 },
+    { score: 50,   min: 400, max: 700 },
+    { score: 20,   min: 500, max: 1000 },
+    { score: 10,   min: 800, max: 1500 }
+];
 
 class Game extends Container{
     constructor(stage) {
@@ -21,7 +35,6 @@ class Game extends Container{
         this.texts = new Texts(this);
 
         this.chickens = [];
-        this.eggs = new Set();
 
         for (let i = 0; i < 5; i++) {
             const chicken = new Chicken(this, i);
@@ -37,7 +50,6 @@ class Game extends Container{
             if(SIGNALS.lives.value > this.lives) {
                 flashBack.style.opacity = '0.5';
                 gsap.to(flashBack.style, {opacity: 0, duration: 0.3, ease: 'power2.inOut'});
-
             } else {
                 gsap.to(stage, {x: 1, repeat: 2, yoyo: true, duration: 0.04, ease: 'power2.inOut', onComplete: () => {
                     stage.x = 0;
@@ -46,8 +58,6 @@ class Game extends Container{
 
             this.lives = SIGNALS.lives.value
         })
-
-
 
         effect(() => {
             if(SIGNALS.lives.value >= 0) return;
@@ -69,49 +79,12 @@ class Game extends Container{
 
     loop(){
         if(SIGNALS.lives.value < 0) return;
-
         const score = SIGNALS.score.value;
-        let min = 1000, max = 2000;
-        if(score > 1000) {
-            min = 180;
-            max = 250;
-        }else if(score > 750){
-            min = 200;
-            max = 290;
-        }else if(score > 500) {
-            min = 220;
-            max = 300;
-        }else if(score > 350) {
-            min = 250;
-            max = 400;
-        } else if(score > 250) {
-            min = 270;
-            max = 450;
-        } else if(score > 180) {
-            min = 280;
-            max = 500;
-        } else if(score > 120) {
-            min = 290;
-            max = 550;
-        } else if(score > 80) {
-            min = 300;
-            max = 600;
-        } else if(score > 50) {
-            min = 400;
-            max = 700;
-        } else if(score > 20) {
-            min = 500;
-            max = 1000;
-        } else if(score > 10) {
-            min = 800;
-            max = 1500;
-        }
+        const {min = 1000, max = 2000} = DELAY_THRESHOLDS.find(i => score > i.score) ?? {};
+
         this.loopTween = gsap.delayedCall(getRandomInt(min, max) / 1000, () => {
             if(SIGNALS.lives.value < 0) return;
-
-
-            const egg = randomFromArr(this.chickens).getEgg();
-            this.eggs.add(egg);
+            randomFromArr(this.chickens).createEgg();
             this.loop();
         })
     }
